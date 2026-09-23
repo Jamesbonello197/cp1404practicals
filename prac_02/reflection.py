53326960d57c46c9d6461822f580fab58833b844
James <bonellojames1@gmail.com>
2026-09-23 18:46:00 +1000

    Answered reflection questions on week1/2

---
+++ prac_02/reflection.py
@@ -0,0 +1,25 @@
+# CP1404 Practical Reflection
+
+## What percentage of the lecture videos have you watched so far?
+
+I have watched 100% of the lecture videos so far.
+
+## What time, location and duration have you scheduled to watch lecture videos?
+
+Since I have work on mondays and multiple subjects that have online videos I have scheduled my lecture watching to be over the weekend
+
+## What are you doing well in the subject so far that you will keep doing?
+
+I believe I am doing well at managing my time so far and getting the content done while trying to really pay attention to the videos
+
+## What do you need to stop or change in your work for this subject overall so far?
+
+I dont think I need to change anything, however I think I should review all the check points/learning outcomes.
+
+## What are you currently doing poorly for practicals in this subject that you will change?
+
+Nothing poorly, I just need to keep practising and making should I learn how to do the best code quality wise.
+
+## What are you doing really well for practicals in this subject?
+
+I complete all tasks with little to no help from any AI.
