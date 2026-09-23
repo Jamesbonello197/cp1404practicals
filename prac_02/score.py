35926af6c27c2bbf485676a716f483802bc5601e
James <bonellojames1@gmail.com>
2026-09-23 17:55:07 +1000

    Added menu with different options

--- prac_02/score.py
+++ prac_02/score.py
@@ -5,16 +5,31 @@ Program to determine score status
 
 import random
 
+MENU = """(G)et a valid score (must be 0-100)
+(P)rint result
+(S)how stars
+(Q)uit"""
+
 
 def main():
-    result = get_score_status()
-    print(f"User score is: {result}")
-    if result == "Excellent":
-        print("You win a prize!")
-    print(get_random_score_status())
+    print(MENU)
+    choice = input(">").upper()
+    while choice != "Q":
+        if choice == "G":
+            get_score_status()
+        elif choice == "P":
+            result = get_score_status()
+            print(f"User score is: {result}")
+        elif choice == "S":
+            score = float(input("Enter score: "))
+            print(int(score) * "*")
+        else:
+            print("Invalid")
+        print(MENU)
+        choice = input().upper()
 
 
-def get_score_status() -> str:
+def get_score_status():
     score = float(input("Enter score: "))
     if score < 0 or score > 100:
         return "Invalid score"
