b12f6c0e3572b4ec170bea6acae553becf07eac9
James <bonellojames1@gmail.com>
2026-09-23 16:01:31 +1000

    Replaced temperature and score input conversion with functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,37 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    print(f"User score is: {get_score_status()}")
+    print(get_random_score_status())
+
+
+def get_score_status() -> str:
+    score = float(input("Enter score: "))
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def get_random_score_status():
+    random_score = random.randint(1, 100)
+    print(f"Random : {random_score}", end=" = ")
+    if random_score < 0 or random_score > 100:
+        return "Invalid score"
+    elif random_score >= 90:
+        return "Excellent"
+    elif random_score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
